7fb36b905d30207a95a976f1bc03375a769742bb
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-11-29 15:22:46 +0100

    Validation rules - max_length

--- fastkit_core/services/validation/rules.py
+++ fastkit_core/services/validation/rules.py
@@ -4,3 +4,7 @@ from pydantic import Field
 def min_length(length: int):
     """Minimum string length."""
     return Field(min_length=length)
+
+def max_length(length: int):
+    """Maximum string length."""
+    return Field(max_length=length)
