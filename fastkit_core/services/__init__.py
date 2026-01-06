da49dd343d07c681a1166a7ab1d6f5b7f79174b3
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-01-06 07:36:38 +0100

    AsyncBaseCrudService

--- fastkit_core/services/__init__.py
+++ fastkit_core/services/__init__.py
@@ -1,5 +1,7 @@
 from fastkit_core.services.base_crud_service import BaseCrudService
+from fastkit_core.services.async_base_crud_service import AsyncBaseCrudService
 
 __all__ = [
-    'BaseCrudService'
+    'BaseCrudService',
+    'AsyncBaseCrudService'
 ]
