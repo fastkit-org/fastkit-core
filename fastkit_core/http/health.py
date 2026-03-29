7666b4fb2451bf0945acd6ff224fac484e6ec02c
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-03-29 19:21:41 +0200

    Health Check - create_health_router

--- fastkit_core/http/health.py
+++ fastkit_core/http/health.py
@@ -1,9 +1,18 @@
-from typing import Literal, List, Optional
+from typing import Literal, List, Optional, Callable, Awaitable
 import asyncio
-import time
 from fastapi import APIRouter, Response, status
 from pydantic import BaseModel, Field
 from fastkit_core.database import health_check_all, health_check_all_async
+from starlette.responses import JSONResponse
+import importlib.metadata
+from functools import lru_cache
+
+@lru_cache
+def get_version() -> str:
+    try:
+        return importlib.metadata.version("fastkit-core")
+    except importlib.metadata.PackageNotFoundError:
+        return "0.0.0-unknown"
 
 class HealthCheck(BaseModel):
     name: str
@@ -36,3 +45,47 @@ async def check_async_databases() -> List[HealthCheck]:
         ))
     return results
 
+
+def create_health_router(
+        checks: list[Callable[[], Awaitable[HealthCheck]]] | None = None,
+        include_db: bool = True,
+        include_version: bool = True,
+        liveness_path: str = '',  # Mounts at prefix root
+        readiness_path: str = '/ready',
+        is_db_async: bool = True,
+) -> APIRouter:
+    router = APIRouter()
+    custom_checks = checks or []
+    app_version = get_version() if include_version else None
+
+    @router.get(liveness_path)
+    async def liveness():
+        return HealthResponse(status="ok", version=app_version)
+
+    @router.get(readiness_path)
+    async def readiness():
+        all_results = []
+        if custom_checks:
+            all_results = await asyncio.gather(*(c() for c in custom_checks))
+
+        if include_db:
+
+            if is_db_async:
+                db_results = await check_async_databases()
+            else:
+                db_results = check_databases()
+
+            all_results.extend(db_results)
+
+        is_failed = any(r.status == 'error' for r in all_results)
+
+        return JSONResponse(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE if is_failed else status.HTTP_200_OK,
+            content=HealthResponse(
+                status= "error" if is_failed else "ok",
+                checks= [r.dict() for r in all_results],
+                version= app_version
+            )
+        )
+
+    return router
